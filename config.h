fffd95d832c875a820ad5a2869f672061464c99c
GliGli <sfx.gligli@free.fr>
2018-11-22 17:27:52 +0100

    File needed for vs2017 compilation

---
+++ config.h
@@ -0,0 +1,129 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `floor' function. */
+#define HAVE_FLOOR 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <random> header file. */
+/* #undef HAVE_RANDOM */
+
+/* Define to 1 if you have the `sqrt' function. */
+#define HAVE_SQRT 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strtol' function. */
+#define HAVE_STRTOL 1
+
+/* Define to 1 if you have the `strtoull' function. */
+#define HAVE_STRTOULL 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <tr1/random> header file. */
+/* #undef HAVE_TR1_RANDOM */
+
+/* Define to 1 if you have the <tr1/unordered_set> header file. */
+/* #undef HAVE_TR1_UNORDERED_SET */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <unordered_set> header file. */
+#define HAVE_UNORDERED_SET 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+/* #undef HAVE__BOOL */
+
+/* Name of package */
+#define PACKAGE "yakmo"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ynaga@tkl.iis.u-tokyo.ac.jp"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "yakmo"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "yakmo 2015-01-21"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "yakmo"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2015-01-21"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* float as floating-point numbers */
+#define USE_FLOAT 1
+
+/* hash support */
+#define USE_HASH 1
+
+/* mt19937 support */
+/* #undef USE_MT19937 */
+
+/* timer support */
+/* #undef USE_TIMER */
+
+/* hash support (TR1) */
+/* #undef USE_TR1_HASH */
+
+/* mt19937 support */
+/* #undef USE_TR1_MT19937 */
+
+/* Version number of package */
+#define VERSION "2015-01-21"
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
